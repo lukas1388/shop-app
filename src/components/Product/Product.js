0d2ebbc812eb64e9e32d825fb86e646f45c6657e
Łukasz Staniszewski <lukas13.88@gmail.com>
2022-06-03 12:17:32 +0200

    Change code in project - Step 4

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -31,13 +31,13 @@ const Product = ({ name, title, basePrice, colors, sizes}) => {
           <div className={styles.sizes}>
             <h3 className={styles.optionLabel}>Sizes</h3>
             <ul className={styles.choices}>
-            {sizes.map(size => <li key={shortid()}><button type="button" className={clsx(size.name === currentSize && styles.active)}>{size.name}</button></li>)}
+            {sizes.map(size => <li key={shortid()}><button type="button" onClick={() => {setCurrentSize(size.name)}} className={clsx(size.name === currentSize && styles.active)}>{size.name}</button></li>)}
             </ul>
           </div>
           <div className={styles.colors}>
             <h3 className={styles.optionLabel}>Colors</h3>
             <ul className={styles.choices}>
-            {colors.map(color => <li key={shortid()}><button type="button" className={clsx(prepareColorClassName(color), color === currentColor && styles.active)} /></li>)}
+            {colors.map(color => <li key={shortid()}><button type="button" onClick={() => {setCurrentColor(color)}} className={clsx(prepareColorClassName(color), color === currentColor && styles.active)} /></li>)}
             </ul>
           </div>
           <Button className={styles.button}>
