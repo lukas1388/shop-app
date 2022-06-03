fb2d1e1b806dda000b4f02600fea4810ec32bf7b
Łukasz Staniszewski <lukas13.88@gmail.com>
2022-06-03 11:41:36 +0200

    Change code in project - Step 1

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,8 +1,9 @@
 import styles from './Product.module.scss';
 import clsx from 'clsx';
 import Button from '../Button/Button';
+import PropTypes from 'prop-types';
 
-const Product = props => {
+const Product = ({ title, basePrice }) => {
   return (
     <article className={styles.product}>
       <div className={styles.imageContainer}>
@@ -13,8 +14,8 @@ const Product = props => {
       </div>
       <div>
         <header>
-          <h2 className={styles.name}>Kodilla shirt</h2>
-          <span className={styles.price}>Price: 20$</span>
+          <h2 className={styles.name}>{title}</h2>
+          <span className={styles.price}>Price: {basePrice}$</span>
         </header>
         <form>
           <div className={styles.sizes}>
@@ -41,6 +42,14 @@ const Product = props => {
       </div>
     </article>
   )
+};
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  basePrice: PropTypes.number.isRequired,
+  colors: PropTypes.array.isRequired,
+  sizes: PropTypes.array.isRequired,
 };
 
 export default Product;
