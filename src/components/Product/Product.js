882948eee8efa1c025cd73cb3728562eb45d0351
Łukasz Staniszewski <lukas13.88@gmail.com>
2022-06-03 11:57:20 +0200

    Change code in project - Step 2

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,15 +2,20 @@ import styles from './Product.module.scss';
 import clsx from 'clsx';
 import Button from '../Button/Button';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
-const Product = ({ title, basePrice }) => {
+const Product = ({ name, title, basePrice, colors, sizes}) => {
+
+  const [currentColor, setCurrentColor] = useState(colors[0]);
+  const [currentSize, setCurrentSize] = useState(sizes[0].name);
+
   return (
     <article className={styles.product}>
       <div className={styles.imageContainer}>
         <img 
           className={styles.image}
-          alt="Kodilla shirt"
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`} />
+          alt={title}
+          src={`${process.env.PUBLIC_URL}/images/products/shirt-${name}--${currentColor}.jpg`} />
       </div>
       <div>
         <header>
